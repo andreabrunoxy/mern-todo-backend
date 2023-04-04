01022c78585d778a1aca30412fb63184f7737e8f
Andrea <andreabrunoxy@gmail.com>
2023-04-04 12:37:24 +0200

    add cors origin render

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(
   cors({
-    origin: ["http://localhost:3000"]
+    origin: ["http://localhost:3000", "https://mern-todo-vxf3.onrender.com"]
   })
 )
 app.use(taskRoutes)
